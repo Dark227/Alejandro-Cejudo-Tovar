b4391796698f4b888d934bf9b3257ecf4aa76319
Dark227 <111063020+Dark227@users.noreply.github.com>
2022-09-13 03:33:14 -0500

    DM Evaluación Práctica de Primer Parcial
    Crea tu app de forma individual con las siguientes características (Valor total 30 puntos)
    
    Icono personalizado con tus iniciales (valor 2 puntos)
    Login (Realiza la validación) (valor 2 puntos)
    Si el usuario da clic en cancelar salir del APP (Muestra en el Toast Hasta Pronto)
    Si el usuario da clic en ingresar
    Valida Usuario (DM1p22B)
    Valida password (Tu Nombre)
    En el Activity Bienvenida, muestra (Todos Image Button) (valor 2 puntos)
    Acerca de…  (es una fotografía tuya)
    Televisión
    Galería 
    Cámara
    Linterna
    Pestanias
    Salir
    Botón Acerca de: Muestra los créditos en un pop (valor 2 puntos)
    Botón Televisión: Permite Visualizar un video, que el usuario seleccione (de tres que hay teniendo control de ellos) (valor 3 puntos)
    Botón Galería: Permite Visualizar un grupo de imágenes, que el usuario seleccione entre 10 y 15  (no mas de 15 no menos de 10) contenidas en la galería del dispositivo móvil, mostrarlas en un list (valor 3 puntos)
    Botón Cámara, Permite tomar una fotografía y mostrarla en el Activity, almacenándola en la galería del dispositivo. (valor 3 puntos)
    Botón Linterna, Permite encender o apagar la linterna del dispositivo móvil (valor 3 puntos)
    Botón Pestanias, Permite la visualización de diferentes características empleando TabLayout (3 pestañas) Pestaña uno muestra una Progres Bar, Pestaña dos, muestra la Ubicación GPS Actual, Pestaña tres Muestra la Hora Actual, actualizando constantemente HH:MM:SS. (valor 3 puntos)
    Botón Salir, Permite terminar la aplicación por completo, muestra un Toast Terminando Aplicación (valor 2 puntos)
    Cada Activity tiene un botón que regresa al Activity de menú (valor 1 punto)
    Realice las validaciones pertinentes, otorgue los permisos de hardware, lectura escritura necesarios. (valor 2 puntos)
    Realice la app de forma responsiva (orientación horizontal o vertical funciona) (valor 2 puntos)

---
+++ Menu.java
@@ -0,0 +1,121 @@
+package com.example.evalucacionprimerparcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Menu extends AppCompatActivity {
+
+    private ImageButton btnAcercaDe;
+    private ImageButton btnTelevision;
+    private ImageButton btnGaleria;
+    private ImageButton btnCamara;
+    private ImageButton btnLinterna;
+    private ImageButton btnPestanias;
+    private ImageButton btnSalida;
+
+    private boolean encendia = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        btnAcercaDe = findViewById(R.id.btnMenuAcercaDe);
+        btnTelevision = findViewById(R.id.btnMenuTelevision);
+        btnGaleria = findViewById(R.id.btnMenuGaleria);
+        btnCamara = findViewById(R.id.btnMenuCamara);
+        btnLinterna = findViewById(R.id.btnMenuLinterna);
+        btnPestanias = findViewById(R.id.btnMenuPestanias);
+        btnSalida = findViewById(R.id.btnMenuSalida);
+
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        final String[] cameraId = {null};
+
+        btnAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCreditos();
+            }
+        });
+
+        btnTelevision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Television.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Galeria.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLinterna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!encendia)
+                {
+                    try
+                    {
+                        cameraId[0] = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraId[0], true);
+                        encendia = true;
+
+                    }catch (CameraAccessException e){
+                        e.printStackTrace();
+                    }
+                }else{
+                    try
+                    {
+                        cameraId[0] = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraId[0], false);
+                        encendia = false;
+                    }catch (CameraAccessException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, CamaraP.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPestanias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, Pestanias.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getCreditos() {
+        new AlertDialog.Builder(this)
+                .setTitle("Acerca de")
+                .setMessage(""+
+                        "Alejandro Cejudo Tovar\n"
+                        + "Edad: 22 anios\n"
+                        + "Profesora: Rocio Elizabeth Alba\n"
+                        + "Movil 2022B\n"
+                        + "vrs 1\n"
+                        + "Evaluacion 1mer Parcial")
+                .setPositiveButton("Aceptar",null)
+                .show();
+    }
+}
